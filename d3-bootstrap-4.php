a4e8ac2af8afaed7ec31e8cc37c2ffe2a3f64eee
Filippo Groppi <filogro@gmail.com>
2022-12-30 22:11:57 +0100

    Aggiunto submenu

--- d3-bootstrap-4.php
+++ d3-bootstrap-4.php
@@ -2,7 +2,7 @@
 
 /**
 * Plugin Name: D3 Bootstrap 4
-* Plugin URI: https://www.derved.com/wp-plugins/d3-bootstrap-4
+* Plugin URI: https://www.derved.com/wp-plugins/d3-suite/d3-bootstrap-4
 * Description: Init Bootstrap 4 to Wordpress.
 * Version: 0.1
 * Author: DERVED®
@@ -22,3 +22,63 @@ function init_d3_bootstrap_4() {
 }
 
 add_action( 'wp_head', 'init_d3_bootstrap_4' );
+
+
+/**
+ * Submenu Content
+ */
+
+function print_d3_bootstrap_4()  {
+    include("templates/d3-submenu-page-content.php");
+}
+
+
+/**
+ * Submenu Page
+ */
+
+function d3_bootstrap_4_admin_submenu()  {
+    add_submenu_page(
+        'd3-suite',
+        'D3 Bootstrap 4', // page title
+        'D3 Bootstrap 4', // menu title
+        'manage_options', // capability
+        'd3-bootstrap-4', // menu slug
+        'print_d3_bootstrap_4'  // callback function
+    );
+}
+
+/**
+ * Menu Content
+ */
+
+function d3_bootstrap_4_print_d3_suite()  {
+    include("templates/d3-suite-page-content.php");
+}
+
+/**
+ * Menu Page
+ */
+
+function d3_bootstrap_4_admin_menu()
+{
+    global $menu;
+    $menuExist = false;
+    foreach ($menu as $item) {
+        if (strtolower($item[0]) == strtolower('D3 Suite')) {
+            $menuExist = true;
+        }
+    }
+    if (!$menuExist) {
+        add_menu_page(
+            'D3 Suite', // page title
+            'D3 Suite', // menu title
+            'manage_options', // capability
+            'd3-suite', // menu slug
+            'd3_bootstrap_4_print_d3_suite'  // callback function
+        );
+    }
+    d3_bootstrap_4_admin_submenu();
+}
+
+add_action( 'admin_menu', 'd3_bootstrap_4_admin_menu' );
---
+++ templates/d3-submenu-page-content.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<br>";
+echo "<h1>D3 Bootstrap 4</h1>";
+echo "<span>D3 Bootstrap 4 is active on your site.</span>";
+echo "<br><br>";
+echo "<span>D3 Bootstrap 4 init Bootstrap 4</span>";
+echo "<br>";
+echo "<a href='https://www.derved.com/wp-plugins/d3-suite/d3-bootstrap-4' target='_blank'>https://www.derved.com/wp-plugins/d3-suite/d3-bootstrap-4</a>";
+
